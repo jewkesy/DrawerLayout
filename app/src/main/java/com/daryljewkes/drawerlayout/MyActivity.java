b96998be2f0b2b8589833b86dc41af1cb8f197b8
Daryl Jewkes <github@daryljewkes.com>
2014-09-16 10:02:22 +0100

    completed part 4

--- app/src/main/java/com/daryljewkes/drawerlayout/MyActivity.java
+++ app/src/main/java/com/daryljewkes/drawerlayout/MyActivity.java
@@ -1,17 +1,23 @@
 package com.daryljewkes.drawerlayout;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -19,8 +25,9 @@ public class MyActivity extends ActionBarActivity implements AdapterView.OnItemC
 
     private DrawerLayout drawerLayout;
     private ListView listView;
-    private String[] planets;
+//    private String[] planets;
     private ActionBarDrawerToggle drawerListener;
+    private MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +35,11 @@ public class MyActivity extends ActionBarActivity implements AdapterView.OnItemC
         setContentView(R.layout.activity_my);
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        planets = getResources().getStringArray(R.array.planets);
+//        planets = getResources().getStringArray(R.array.planets);
         listView = (ListView) findViewById(R.id.drawerListLeft);
-        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, planets));
+//        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, planets));
+        myAdapter = new MyAdapter(this);
+        listView.setAdapter(myAdapter);
         listView.setOnItemClickListener(this);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         drawerListener = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close){
@@ -91,16 +100,65 @@ public class MyActivity extends ActionBarActivity implements AdapterView.OnItemC
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, planets[position] + " was selected", Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, planets[position] + " was selected", Toast.LENGTH_LONG).show();
         selectItem(position);
     }
 
     public void selectItem(int position) {
         listView.setItemChecked(position, true);
-        setTitle(planets[position]);
+//        setTitle(planets[position]);
     }
 
     public void setTitle(String title) {
         getSupportActionBar().setTitle(title);
     }
+
 }
+
+class MyAdapter extends BaseAdapter {
+    private Context context;
+    String[] socialSites;
+    int[] images = {R.drawable.ic_facebook, R.drawable.ic_twitter, R.drawable.ic_google_plus};
+
+    public MyAdapter(Context context){
+        this.context = context;
+        socialSites = context.getResources().getStringArray(R.array.social);
+    }
+    @Override
+    public int getCount() {
+        return socialSites.length;
+//        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return socialSites[position];
+//        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+//        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = null;
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.custom_row, parent, false);
+        }
+        else {
+            row = convertView;
+        }
+
+        TextView titleTextView = (TextView) row.findViewById(R.id.textView1);
+        ImageView titleImageView = (ImageView) row.findViewById(R.id.imageView1);
+        titleTextView.setText(socialSites[position]);
+        titleImageView.setImageResource(images[position]);
+
+        return row;
+    }
+}
+
